9735c24d0d8950c579bbc648d4f9b229c2eb3ab9
Matt Gmur <mattgmur@SDMacBookPro2.local>
2017-04-14 22:22:17 -0600

    building equation array

--- calculator.js
+++ calculator.js
@@ -1,47 +1,88 @@
 $(document).ready(function() {
-  config();
-  listen();
+    config();
+    listen();
 });
 
-// buttonArr size = 20
 var BUTTONS_PER_ROW = 4;
-var buttonArr = ['AC', '+/-', '%', '÷',
-                  7, 8, 9, 'x', 4, 5, 6,
-                  '-', 1, 2, 3, '+', 0,
-                  '.', '√', '='];
+var ERR_MSG = "Invalid input";
+var buttonArr = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6,
+    '-', 1, 2, 3, '+', 0, '.', '√', '='
+];
 
-var config = function(){
+var config = function() {
 
-  var counter = 0;
+    var counter = 0;
 
-  for(var i = 0; i < (buttonArr.length/BUTTONS_PER_ROW); i++) {
+    for (var i = 0; i < (buttonArr.length / BUTTONS_PER_ROW); i++) {
 
-    var $buttonRow = $('<div>');
-    $buttonRow.addClass('buttonRow');
+        var $buttonRow = $('<div>');
+        $buttonRow.addClass('buttonRow');
 
-    for(var j = 0; j < BUTTONS_PER_ROW; j++){
-      var $button = $('<button>');
-      $button.attr('id', counter);
-      $button.attr('name', buttonArr[counter]);
-      $button.addClass('calcButton');
-      $button.addClass('btn');
-      $button.text(buttonArr[counter]);
-      $buttonRow.append($button);
-      counter++;
+        for (var j = 0; j < BUTTONS_PER_ROW; j++) {
+            var $button = $('<button>');
+
+            $button.attr('id', counter);
+            $button.attr('name', buttonArr[counter]);
+            $button.addClass('calcButton');
+            $button.addClass('btn');
+            $button.text(buttonArr[counter]);
+            $buttonRow.append($button);
+
+            counter++;
+        }
+
+        $('#calc').append($buttonRow);
     }
-    $('#calc').append($buttonRow);
-  }
 }
 
 var listen = function() {
 
-    $('.calcButton').on('click', function(e){
+    $('.calcButton').on('click', function(e) {
 
-      e.preventDefault();
+        e.preventDefault();
 
-      var btnName = this.name;
-      var btnId = this.id;
+        buildEquation(this.name, parseInt(this.id));
+    });
+}
 
+var equation = [];
 
-  });
+var buildEquation = function(name, index) {
+
+    switch (index) {
+        case 0:
+            equation = [];
+            break;
+        case 19:
+            console.log(equation);
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 7:
+        case 11:
+        case 15:
+        case 18:
+            equation.push(name)
+            break;
+        default:
+            var lastEntry = equation[equation.length - 1];
+
+            if (isNaN(lastEntry)) {
+                equation.push(name);
+            } else {
+                equation[equation.length - 1] += name;
+            }
+    }
+
+    appendEquation();
+
+}
+
+var appendEquation = function() {
+
+    $eq = $('<div>');
+    $eq.text(equation);
+    $('#calc').append($eq);
+
 }
